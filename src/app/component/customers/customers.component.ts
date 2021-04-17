46c65565733f91bf4315231ead61a45b499b2a8a
hamzakhan <hamza_beetechnica@gmail.com>
2021-04-17 13:16:58 +0500

    content updated

--- src/app/component/customers/customers.component.ts
+++ src/app/component/customers/customers.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomersComponent implements OnInit {
 
-  showDiv: number = 1
+  showDiv: number = 2
 
   TeamMembers  = [
     {
