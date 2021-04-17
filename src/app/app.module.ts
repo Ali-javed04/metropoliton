47106af50b0a27480ef3424ade4af95ed83a0ff1
hamzakhan <hamza_beetechnica@gmail.com>
2021-04-17 12:32:00 +0500

    module resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './component/main/main.component';
@@ -14,19 +13,6 @@ import { CustomersComponent } from './component/customers/customers.component';
 import { ContactUsComponent } from './component/contact-us/contact-us.component';
 import { CompanyStoreComponent } from './component/company-store/company-store.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
-
-
-var firebaseConfig = {
-  apiKey: "AIzaSyAAwuSIW39GpY7n7XALI3m2kXYhoCke3Ng",
-  authDomain: "isway-mkrt-websites.firebaseapp.com",
-  projectId: "isway-mkrt-websites",
-  storageBucket: "isway-mkrt-websites.appspot.com",
-  messagingSenderId: "762069750000",
-  appId: "1:762069750000:web:a04c283973890963263235",
-  measurementId: "G-GJ5JY5ZEY7"
-};
-
-firebase.initializeApp(firebaseConfig);
 import { RegistrationComponent } from './registration/registration.component';
 import firebase from 'firebase';
 
